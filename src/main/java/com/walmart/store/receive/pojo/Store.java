9d16c662d7d40ca83713dc5806b7949b2f59ba4a
s0s03ha <s0s03ha@homeoffice.wal-mart.com>
2019-04-17 15:49:13 +0530

    Adding pojo files

--- src/main/java/com/walmart/store/receive/pojo/Store.java
+++ src/main/java/com/walmart/store/receive/pojo/Store.java
@@ -1,13 +1,13 @@
 package com.walmart.store.receive.pojo;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigInteger;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
@@ -17,24 +17,43 @@ import java.util.Date;
 @Document(collection = "receive-summary")
 public class Store {
 
-       // private ObjectId _id;
-        @Id
-        private String _id;
+        private char receivingControlNumber;
+        private int storeNumber;
+        private int accountNumber;
+        private int controlSequenceNumber;
+        private Integer receiveSequenceNumber;
+        private char matchIndicator;
+        private Double totalCostAmount;
+        private Double totalRetailAmount;
+        private Integer freightBillId;
         private Integer poReceiveId;
-        private Integer purchaseOrderId;
-        private char transactionType;
-        private String controlNumber;
-        private Integer destinationBusinessUnitId;
-        private Integer baseDivisionNumber;
+        private Integer controlType;
+        private Integer poId;
+        private Integer transactionType;
+        private int baseDivisionNumber;
         private Date finalDate;
         private Time finalTime;
+        private char claimPendingIndicator;
+        private char freeAstrayIndicator;
+        private char freightConslIndicator;
+        private Double receiveWeightQuantity;
         private char businessStatusCode;
+        private List<ReceiveMDS> receiveMDS;
+        private List<ReceiveExpense> receiveExpense;
+        private char departmentNumber;
+        private Integer casesReceived;
+        private Timestamp finalizedLoadTimestamp;
+        private Integer finalizedUpdateSequenceNumber;
         private BigInteger freightBillExpandId;
         private Timestamp initialReceiveTimestamp;
         private Date mdsReceiveDate;
+        private Timestamp creationDate;
+        private Timestamp lastUpdatedDate;
+        private char createChannelOrSource;
+        private char cosmosDB2SyncStatus;
+        private char warehouseStoreIndicator;
         private Date receiveProcessDate;
         private char sequenceNumber;
-        private Integer mdseConditionCode;
         private Integer vendorNumber;
         private Date loadTimestamp;
         private char mdseQuantityUnitOfMeasureCode;
