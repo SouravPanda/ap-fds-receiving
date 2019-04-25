a09281f6d8bcb742b6489506747cef5bcf77e273
s0s03ha <s0s03ha@homeoffice.wal-mart.com>
2019-04-25 10:35:23 +0530

    Adding changes with respect to new payload

--- src/main/java/com/walmart/store/receive/mapper/WarehouseMapper.java
+++ src/main/java/com/walmart/store/receive/mapper/WarehouseMapper.java
@@ -1,143 +1,87 @@
-        package com.walmart.store.receive.mapper;
+            package com.walmart.store.receive.mapper;
 
-        import com.walmart.store.receive.pojo.ReceiveExpense;
-        import com.walmart.store.receive.pojo.ReceiveMDS;
-        import com.walmart.store.receive.pojo.Store;
-        import org.springframework.stereotype.Component;
+            import com.walmart.store.receive.pojo.ReceiveExpense;
+    import com.walmart.store.receive.pojo.ReceiveMDS;
+    import com.walmart.store.receive.pojo.Warehouse;
+    import org.springframework.stereotype.Component;
 
-        import java.sql.Time;
-        import java.sql.Timestamp;
-        import java.util.Date;
-        import java.util.List;
+    import java.sql.Timestamp;
+    import java.util.Date;
+    import java.util.List;
 
-        @Component
-        public class WarehouseMapper {
+            @Component
+            public class WarehouseMapper {
 
-            public Store getStores(Store store) {
-                int vendorNumber=store.getVendorNumber();
-                if(vendorNumber!=0){
-                    store.setVendorNumber(vendorNumber);
+                public Warehouse getWarehouse(Warehouse warehouse) {
+                    int vendorNumber = warehouse.getVendorNumber();
+                    if (vendorNumber != 0) {
+                        warehouse.setVendorNumber(vendorNumber);
+                    }
+                    char receivingControlNumber = warehouse.getReceivingControlNumber();
+                    if (receivingControlNumber != 0) {
+                        warehouse.setReceivingControlNumber(receivingControlNumber);
+                    }
+                    Integer freightBillId = warehouse.getFreightBillId();
+                    if (freightBillId != null) {
+                        warehouse.setFreightBillId(freightBillId);
+                    }
+                    Integer poReceiveId = warehouse.getPoReceiveId();
+                    if (poReceiveId != null) {
+                        warehouse.setPoReceiveId(poReceiveId);
+                    }
+                    Integer poId = warehouse.getPoId();
+                    if (poId != null) {
+                        warehouse.setPoId(poId);
+                    }
+                    char businessStatusCode = warehouse.getBusinessStatusCode();
+                    if (businessStatusCode != 0) {
+                        warehouse.setBusinessStatusCode(businessStatusCode);
+                    }
+                    Date receiveProcessDate = warehouse.getReceiveProcessDate();
+                    if (null != receiveProcessDate) {
+                        warehouse.setReceiveProcessDate(receiveProcessDate);
+                    }
+                    Double receiveWeightQuantity = warehouse.getReceiveWeightQuantity();
+                    if (receiveWeightQuantity != 0) {
+                        warehouse.setReceiveWeightQuantity(receiveWeightQuantity);
+                    }
+                    Timestamp finalizedLoadTimestamp = warehouse.getFinalizedLoadTimestamp();
+                    if (finalizedLoadTimestamp != null) {
+                        warehouse.setFinalizedLoadTimestamp(finalizedLoadTimestamp);
+                    }
+                    Integer finalizedUpdateSequenceNumber = warehouse.getFinalizedUpdateSequenceNumber();
+                    if (finalizedUpdateSequenceNumber != 0) {
+                        warehouse.setFinalizedUpdateSequenceNumber(finalizedUpdateSequenceNumber);
+                    }
+                    List<ReceiveMDS> receiveMDS = warehouse.getReceiveMDS();
+                    if (!receiveMDS.isEmpty()) {
+                        warehouse.setReceiveMDS(receiveMDS);
+                    }
+                    List<ReceiveExpense> receiveExpense = warehouse.getReceiveExpense();
+                    if (!receiveExpense.isEmpty()) {
+                        warehouse.setReceiveExpense(receiveExpense);
+                    }
+                    Timestamp creationDate = warehouse.getCreationDate();
+                    if (creationDate != null) {
+                        warehouse.setCreationDate(creationDate);
+                    }
+                    Timestamp lastUpdatedDate = warehouse.getLastUpdatedDate();
+                    if (lastUpdatedDate != null) {
+                        warehouse.setLastUpdatedDate(lastUpdatedDate);
+                    }
+                    char createChannelOrSource = warehouse.getCreateChannelOrSource();
+                    if (createChannelOrSource != 0) {
+                        warehouse.setCreateChannelOrSource(createChannelOrSource);
+                    }
+                    char cosmosDB2SyncStatus = warehouse.getCosmosDB2SyncStatus();
+                    if (cosmosDB2SyncStatus != 0) {
+                        warehouse.setCosmosDB2SyncStatus(cosmosDB2SyncStatus);
+                    }
+                    char warehouseStoreIndicator = warehouse.getWarehouseStoreIndicator();
+                    if (warehouseStoreIndicator != 0) {
+                        warehouse.setWarehouseStoreIndicator(warehouseStoreIndicator);
+                    }
+
+                    return warehouse;
                 }
-                int accountNumber=store.getAccountNumber();
-                if(accountNumber!=0){
-                    store.setAccountNumber(accountNumber);
-                }
-                int controlSequenceNumber=store.getControlSequenceNumber();
-                if(controlSequenceNumber!=0){
-                    store.setControlSequenceNumber(controlSequenceNumber);
-                }
-                char matchIndicator=store.getMatchIndicator();
-                if(matchIndicator!=0){
-                    store.setMatchIndicator(matchIndicator);
-                }
-                Double totalCostAmount=store.getTotalCostAmount();
-                if(totalCostAmount!=null){
-                    store.setTotalCostAmount(totalCostAmount);
-                }
-                Double totalRetailAmount=store.getTotalRetailAmount();
-                if(totalRetailAmount!=null){
-                    store.setTotalRetailAmount(totalRetailAmount);
-                }
-                Integer freightBillId=store.getFreightBillId();
-                if(freightBillId!=null){
-                    store.setFreightBillId(freightBillId);
-                }
-                Integer poReceiveId=store.getPoReceiveId();
-                if(poReceiveId!=null){
-                    store.setPoReceiveId(poReceiveId);
-                }
-                Integer poId=store.getPoId();
-                if(poId!=null){
-                    store.setPoId(poId);
-                }
-                Integer controlType=store.getControlType();
-                if(controlType!=null){
-                    store.setControlType(controlType);
-                }
-                Integer transactionType=store.getTransactionType();
-                if(transactionType!=null){
-                    store.setTransactionType(transactionType);
-                }
-                int baseDivisionNumber=store.getBaseDivisionNumber();
-                if(baseDivisionNumber!=0){
-                    store.setBaseDivisionNumber(baseDivisionNumber);
-                }
-                Date finalDate=store.getFinalDate();
-                if(null!=finalDate) {
-                    store.setFinalDate(finalDate);
-                }
-                    Time finalTime=store.getFinalTime();
-                if(null!=finalTime) {
-                    store.setFinalTime(finalTime);
-                }
-                Integer receiveSequenceNumber=store.getReceiveSequenceNumber();
-                if(null!=receiveSequenceNumber) {
-                    store.setReceiveSequenceNumber(receiveSequenceNumber);
-                }
-                Date loadTimestamp=store.getLoadTimestamp();
-                if(null!=loadTimestamp){
-                    store.setLoadTimestamp(loadTimestamp);
-                }
-                char claimPendingIndicator=store.getClaimPendingIndicator();
-                if(claimPendingIndicator!=0){
-                    store.setClaimPendingIndicator(claimPendingIndicator);
-                }
-                char freeAstrayIndicator=store.getFreeAstrayIndicator();
-                if(freeAstrayIndicator!=0){
-                    store.setFreeAstrayIndicator(freeAstrayIndicator);
-                }
-                char freightConslIndicator=store.getFreightConslIndicator();
-                if(freightConslIndicator!=0){
-                    store.setFreightConslIndicator(freightConslIndicator);
-                }
-                char createChannelOrSource=store.getCreateChannelOrSource();
-                if(createChannelOrSource!=0){
-                    store.setCreateChannelOrSource(createChannelOrSource);
-                }
-                char cosmosDB2SyncStatus=store.getCosmosDB2SyncStatus();
-                if(cosmosDB2SyncStatus!=0){
-                    store.setCosmosDB2SyncStatus(cosmosDB2SyncStatus);
-                }
-                char warehouseStoreIndicator=store.getWarehouseStoreIndicator();
-                if(warehouseStoreIndicator!=0){
-                    store.setWarehouseStoreIndicator(warehouseStoreIndicator);
-                }
-                char sequenceNumber=store.getSequenceNumber();
-                if(sequenceNumber!=0){
-                    store.setSequenceNumber(sequenceNumber);
-                }
-                char mdseQuantityUnitOfMeasureCode=store.getMdseQuantityUnitOfMeasureCode();
-                if(mdseQuantityUnitOfMeasureCode!=0){
-                    store.setMdseQuantityUnitOfMeasureCode(mdseQuantityUnitOfMeasureCode);
-                }
-                List<ReceiveMDS> receiveMDS=store.getReceiveMDS();
-                if(!receiveMDS.isEmpty()){
-                    store.setReceiveMDS(receiveMDS);
-                }
-                List<ReceiveExpense> receiveExpense=store.getReceiveExpense();
-                if(!receiveExpense.isEmpty()){
-                    store.setReceiveExpense(receiveExpense);
-                }
-                Integer casesReceived=store.getCasesReceived();
-                if(casesReceived!=null){
-                    store.setCasesReceived(casesReceived);
-                }
-                Timestamp finalizedLoadTimestamp= store.getFinalizedLoadTimestamp();
-                if(finalizedLoadTimestamp!=null){
-                    store.setFinalizedLoadTimestamp(finalizedLoadTimestamp);
-                }
-                Timestamp initialReceiveTimestamp= store.getInitialReceiveTimestamp();
-                if(initialReceiveTimestamp!=null){
-                    store.setInitialReceiveTimestamp(initialReceiveTimestamp);
-                }
-                Date mdsReceiveDate=store.getFinalDate();
-                if(null!=mdsReceiveDate) {
-                    store.setMdsReceiveDate(mdsReceiveDate);
-                }
-                Date receiveProcessDate=store.getReceiveProcessDate();
-                if(null!=receiveProcessDate) {
-                    store.setReceiveProcessDate(receiveProcessDate);
-                }
-                return store;
             }
-        }
--- src/main/java/com/walmart/store/receive/pojo/Store.java
+++ src/main/java/com/walmart/store/receive/pojo/Store.java
@@ -1,9 +1,11 @@
 package com.walmart.store.receive.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigInteger;
+import javax.validation.constraints.NotEmpty;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Date;
@@ -14,48 +16,86 @@ import java.util.List;
 @Setter
 @ToString
 @NoArgsConstructor
-@Document(collection = "receive-summary")
+@Document(collection = "col2")
 public class Store {
 
+        @Id
+        private String _id;
+        @NotEmpty
+        @JsonProperty("Receiving Control Number")
         private char receivingControlNumber;
+        @NotEmpty
+        @JsonProperty("Store Number")
         private int storeNumber;
+        @NotEmpty
+        @JsonProperty("Account Number")
         private int accountNumber;
+        @JsonProperty("Control Sequence Number")
         private int controlSequenceNumber;
+        @JsonProperty("Receive Sequence Number")
         private Integer receiveSequenceNumber;
+        @NotEmpty
+        @JsonProperty("Match Indicator")
         private char matchIndicator;
+        @JsonProperty("Total Cost Amount")
         private Double totalCostAmount;
+        @JsonProperty("Total Retail Amount")
         private Double totalRetailAmount;
-        private Integer freightBillId;
+        @NotEmpty
+        @JsonProperty("PO Receive ID")
         private Integer poReceiveId;
-        private Integer controlType;
-        private Integer poId;
+        @NotEmpty
+        @JsonProperty("Transaction Type")
         private Integer transactionType;
+        @NotEmpty
+        @JsonProperty("Base Division Number")
         private int baseDivisionNumber;
+        @NotEmpty
+        @JsonProperty("Final Date")
         private Date finalDate;
+        @NotEmpty
+        @JsonProperty("Final Time")
         private Time finalTime;
-        private char claimPendingIndicator;
-        private char freeAstrayIndicator;
-        private char freightConslIndicator;
+        @NotEmpty
+        @JsonProperty("Control Type")
+        private Integer controlType;
+        @JsonProperty("Receive Weight Quantity")
         private Double receiveWeightQuantity;
-        private char businessStatusCode;
+        @NotEmpty
+        @JsonProperty("Receive MDS")
         private List<ReceiveMDS> receiveMDS;
+        @JsonProperty("Receive Expense")
         private List<ReceiveExpense> receiveExpense;
+        @JsonProperty("Department Number")
         private char departmentNumber;
+        @JsonProperty("Cases Received")
         private Integer casesReceived;
+        @JsonProperty("Finalized Load Timestamp")
         private Timestamp finalizedLoadTimestamp;
+        @JsonProperty("Finalized Update Sequence Number")
         private Integer finalizedUpdateSequenceNumber;
-        private BigInteger freightBillExpandId;
+        @JsonProperty("Initial Receive Timestamp")
         private Timestamp initialReceiveTimestamp;
+        @JsonProperty("MDS Receive Date")
         private Date mdsReceiveDate;
+        @JsonProperty("Creation Date")
         private Timestamp creationDate;
+        @JsonProperty("Last Updated Date")
         private Timestamp lastUpdatedDate;
-        private char createChannelOrSource;
+        @JsonProperty("Create/Update Channel or Source")
+        private char USERID;
+        @JsonProperty("cosmos DB2 Sync Status")
         private char cosmosDB2SyncStatus;
+        @JsonProperty("warehouse store indicator")
         private char warehouseStoreIndicator;
+        @JsonProperty("Receive Process Date")
         private Date receiveProcessDate;
+        @JsonProperty("Sequence Number")
         private char sequenceNumber;
+        @NotEmpty
+        @JsonProperty("Vendor Number")
         private Integer vendorNumber;
-        private Date loadTimestamp;
-        private char mdseQuantityUnitOfMeasureCode;
+        private Integer recordType;
+        private char countryCode;
 }
 
