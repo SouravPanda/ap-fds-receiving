b084ed9c89d6f705c132f1465be13c2dbdf36692
s0s03ha <s0s03ha@homeoffice.wal-mart.com>
2019-06-10 19:17:01 +0530

    Final changes

--- src/main/java/com/walmart/finance/ap/fds/receiving/SpringReceiveApplication.java
+++ src/main/java/com/walmart/finance/ap/fds/receiving/SpringReceiveApplication.java
@@ -1,12 +1,7 @@
 package com.walmart.finance.ap.fds.receiving;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class SpringReceiveApplication {
