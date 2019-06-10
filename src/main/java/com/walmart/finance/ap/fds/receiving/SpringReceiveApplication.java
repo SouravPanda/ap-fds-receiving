3d946f0d72076dc79a13168a9f40ee773587932a
s0s03ha <s0s03ha@homeoffice.wal-mart.com>
2019-06-10 19:16:40 +0530

    Final changes

--- src/main/java/com/walmart/finance/ap/fds/receiving/SpringReceiveApplication.java
+++ src/main/java/com/walmart/finance/ap/fds/receiving/SpringReceiveApplication.java
@@ -9,16 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
-//@EnableAutoConfiguration
-
 public class SpringReceiveApplication {
-/*
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-*/
 
     public static void main(String[] args) {
 
