325d50e11b7a5739fd7a65fe74ea30678872e8a3
s0s03ha <s0s03ha@homeoffice.wal-mart.com>
2019-06-18 14:04:13 +0530

    Sonar file

--- src/test/java/com/walmart/finance/ap/fds/receiving/controller/ReceivingLineControllerTest.java
+++ src/test/java/com/walmart/finance/ap/fds/receiving/controller/ReceivingLineControllerTest.java
@@ -5,6 +5,7 @@ import com.walmart.finance.ap.fds.receiving.converter.ReceivingLineResponseConve
 import com.walmart.finance.ap.fds.receiving.model.ReceivingLine;
 import com.walmart.finance.ap.fds.receiving.response.ReceivingLineResponse;
 import com.walmart.finance.ap.fds.receiving.service.ReceiveLineServiceImpl;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,9 +15,6 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.web.servlet.MockMvc;
@@ -54,11 +52,11 @@ public class ReceivingLineControllerTest {
     public void getReceiveLineTest() throws Exception {
 
         ReceivingLineResponse receivingLineResponse = new ReceivingLineResponse(0, 0, 0, 0, 0, 2.9,
-                1.9, 0, 0,  "kk",
-                "0","0","0", 0,  0,0);
+                1.9, 0, 0, "kk",
+                "0", "0", "0", 0, 0, 0);
         ReceivingLineResponse receivingLineResponseAt = new ReceivingLineResponse(0, 0, 0, 0, 0, 2.9,
-                1.9, 0, 0,  "kk",
-                "0","0","LL90", 99,  0,0);
+                1.9, 0, 0, "kk",
+                "0", "0", "LL90", 99, 0, 0);
 
         List listOfContent = new ArrayList<ReceivingLine>();
         ReceivingLine receivingLine = new ReceivingLine("112|1804823|8264|18|0|1995-10-17|1995-10-17T18:45:21|122", "4665267",
@@ -66,12 +64,12 @@ public class ReceivingLineControllerTest {
                 89, 12, "1122", 99, 8264, 18,
                 LocalDate.of(1995, 10, 17), LocalDateTime.of(1995, 10, 17, 18, 45, 21), 22,
                 LocalDateTime.of(1990, 10, 17, 18, 45, 21), 'A', "BKP", "111", 0, LocalDate.now(),
-                0, 1.9, "LL",0,"");
+                0, 1.9, "LL", 0, "");
         ReceivingLine receivingLineAt = new ReceivingLine("0|0|0|0|0|null|null|12", "6778", 0,
                 0, 0, 0, 0.0, 0.0, "0", 0,
                 0, "0KKL", 0, 0, 0, null, null, 12,
                 LocalDateTime.of(1985, 10, 17, 18, 45, 21), 'A', "BKP", "111",
-                0, LocalDate.now(), 0, 1.9, "LL",0,"");
+                0, LocalDate.now(), 0, 1.9, "LL", 0, "");
         listOfContent.add(receivingLine);
         listOfContent.add(receivingLineAt);
 
@@ -79,15 +77,14 @@ public class ReceivingLineControllerTest {
         content.add(receivingLineResponse);
         content.add(receivingLineResponseAt);
 
-        PageRequest pageRequest = new PageRequest(1, 1, Sort.unsorted());
-        PageImpl<ReceivingLineResponse> pageImplResponse = new PageImpl(content, pageRequest, 1);
-
-
         when(mongoTemplate.find(Mockito.any(Query.class), Mockito.any(Class.class), Mockito.anyString())).thenReturn(listOfContent);
         when(receivingLineResponseConverter.convert(Mockito.any(ReceivingLine.class))).thenReturn(receivingLineResponse);
 
         Mockito.when(receiveLineServiceImpl.getLineSummary("1145", "1124", "11", "HHLL", "3580",
                 "99")).thenReturn(listOfContent);
+
+        Assert.assertEquals(receiveLineServiceImpl.getLineSummary("1145", "1124", "11", "HHLL", "3580",
+                "99"), listOfContent);
 
     }
 }
