741994a9fe91071edd39151d264c6bc1d1b305f2
Mohammed Mohideen <mohammed.mohideen@walmartlabs.com>
2019-06-18 11:41:51 +0530

    UT Changes for Producer and Event Listener Code. All Test Verified.
    [INFO] Results:
    [INFO]
    [INFO] Tests run: 14, Failures: 0, Errors: 0, Skipped: 0
    [INFO]
    [INFO] ------------------------------------------------------------------------
    [INFO] BUILD SUCCESS
    [INFO] ------------------------------------------------------------------------
    [INFO] Total time: 9.164 s
    [INFO] Finished at: 2019-06-18T11:40:08+05:30
    [INFO] Final Memory: 40M/505M

---
+++ src/test/java/com/walmart/finance/ap/fds/receiving/messageproducer/ProducerTest.java
@@ -0,0 +1,52 @@
+package com.walmart.finance.ap.fds.receiving.messageproducer;
+
+import com.walmart.finance.ap.fds.receiving.model.ReceiveSummary;
+import com.walmart.finance.ap.fds.receiving.model.ReceivingLine;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doThrow;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ProducerTest {
+
+    @Spy
+    @InjectMocks
+    private  Producer producer;
+
+    @Mock
+    private KafkaTemplate kafkaTemplate;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test(expected = Exception.class)
+    public void sendReceiveSummary() {
+
+        ReceiveSummary receiveSummary = new ReceiveSummary("abc","2",2,2,2,null,null,2,2,2,2,2,'A',0.0,0.0,2,'A',2L,'A','A','A',null,null,null,0.0,2,2,2,null,2,"2","A",null,"A",'A',"A");
+        producer.sendReceiveSummary(receiveSummary,"fds-db-dev.test.receive-summary");
+        doThrow(new Exception()).when(producer).sendReceiveSummary(receiveSummary,"fds-db-dev.test.receive-summary");
+    }
+
+    @Test(expected = Exception.class)
+    public void sendReceiveLine() {
+        ReceivingLine receiveLine = new ReceivingLine();
+        receiveLine.setPurchasedOrderId(345);
+        receiveLine.setBaseDivisionNumber(34);
+        receiveLine.set_id("3243434|998945|48545");
+        producer.sendReceiveLine(receiveLine,"fds-db-dev.test.receive-summary");
+        doThrow(new Exception()).when(producer).sendReceiveLine(receiveLine,"fds-db-dev.test.receive-summary");
+    }
+}
