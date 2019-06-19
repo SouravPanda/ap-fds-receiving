466e753389ce0b8998fa1d2576a97f35a04b648e
s0s03ha <s0s03ha@homeoffice.wal-mart.com>
2019-06-19 15:42:54 +0530

    Increasing code coverage

--- src/test/java/com/walmart/finance/ap/fds/receiving/controller/ReceivingSummaryControllerTest.java
+++ src/test/java/com/walmart/finance/ap/fds/receiving/controller/ReceivingSummaryControllerTest.java
@@ -3,10 +3,13 @@ package com.walmart.finance.ap.fds.receiving.controller;
 
 import com.walmart.finance.ap.fds.receiving.converter.ReceivingSummaryResponseConverter;
 import com.walmart.finance.ap.fds.receiving.model.ReceiveSummary;
+import com.walmart.finance.ap.fds.receiving.request.Meta;
 import com.walmart.finance.ap.fds.receiving.request.ReceivingSummaryLineRequest;
 import com.walmart.finance.ap.fds.receiving.request.ReceivingSummaryRequest;
+import com.walmart.finance.ap.fds.receiving.request.SorRoutingCtx;
 import com.walmart.finance.ap.fds.receiving.response.ReceivingSummaryResponse;
 import com.walmart.finance.ap.fds.receiving.response.SuccessMessage;
+import com.walmart.finance.ap.fds.receiving.service.ReceiveSummaryService;
 import com.walmart.finance.ap.fds.receiving.service.ReceiveSummaryServiceImpl;
 import org.junit.Assert;
 import org.junit.Before;
@@ -15,12 +18,16 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -29,13 +36,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(PowerMockRunner.class)
-@WebMvcTest(ReceivingLineController.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(ReceivingSummaryController.class)
 public class ReceivingSummaryControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ReceiveSummaryService receiveSummaryService;
 
     @Mock
     private ReceiveSummaryServiceImpl receiveSummaryServiceImpl;
@@ -111,13 +123,19 @@ public class ReceivingSummaryControllerTest {
 
         Assert.assertEquals(receiveSummaryServiceImpl.getReceiveSummary("US", "77", "8", listOfReceiptNumbers, "66",
                 "99", "675", "987", "18", "WW8", "776"
-                , "1980", "1988-12-12", "1990-12-12", listOfItemNumbers, listOfUpcNumbers).getData(),successMessage.getData());
+                , "1980", "1988-12-12", "1990-12-12", listOfItemNumbers, listOfUpcNumbers).getData(), successMessage.getData());
 
     }
 
     @Test
     public void updateSummaryTest() {
-        ReceivingSummaryRequest receivingSummaryRequest = new ReceivingSummaryRequest("0", "0", LocalDate.now(), 0, "A", null);
+        Meta meta = new Meta();
+        SorRoutingCtx sorRoutingCtx = new SorRoutingCtx();
+        sorRoutingCtx.setInvProcAreaCode(36);
+        sorRoutingCtx.setLocationCountryCd("US");
+        sorRoutingCtx.setReplnTypCd("R");
+        meta.setSorRoutingCtx(sorRoutingCtx);
+        ReceivingSummaryRequest receivingSummaryRequest = new ReceivingSummaryRequest("2", "2", LocalDate.now(), 2, "A", meta);
         List<ReceivingSummaryRequest> responseList = new ArrayList<>();
         SuccessMessage successMessage = new SuccessMessage();
         successMessage.setMessage(true);
@@ -125,13 +143,65 @@ public class ReceivingSummaryControllerTest {
         successMessage.setData(responseList);
         successMessage.setTimestamp(LocalDateTime.now());
         Mockito.when(receiveSummaryServiceImpl.updateReceiveSummary(receivingSummaryRequest, "US")).thenReturn(successMessage);
-        Assert.assertEquals(receiveSummaryServiceImpl.updateReceiveSummary(receivingSummaryRequest,"US").getData(),successMessage.getData());
+        String body = new String("{\n" +
+                "            \"receiptNumber\": \"2\",\n" +
+                "            \"controlNumber\": \"2\",\n" +
+                "            \"receiptDate\": \"2019-04-19\",\n" +
+                "            \"locationNumber\": 2,\n" +
+                "            \"businessStatusCode\": \"A\",\n" +
+                "            \"meta\": {\n" +
+                "                \"unitofWorkid\": null,\n" +
+                "                \"sorRoutingCtx\": {\n" +
+                "                    \"replnTypCd\": null,\n" +
+                "                    \"invProcAreaCode\": 36,\n" +
+                "                    \"locationCountryCd\": \"US\"\n" +
+                "                }\n" +
+                "             }\n" +
+                "}");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/US/receiving/summary")
+                .header("X-FDS-FOUNDATION-API-KEY", "razorbacks")
+                .header("Content-Type", "application/json")
+                .header("X-IBM-Client-Secret", "rK7xP5iW4uV0gI4mA0dM8yQ3dV8tW2hG6nB8uA5mY1kA4sQ8oB")
+                .header("X-IBM-Client-Id", "e684e483-dbba-41d5-a040-6043b48798f5")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(body)
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try {
+            mockMvc.perform(requestBuilder)
+                    .andExpect(content().json(body))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json("{\n" +
+                            "    \"message\": true,\n" +
+                            "    \"timestamp\": \"2019-06-19T14:29:17.812\",\n" +
+                            "    \"data\": [\n" +
+                            "        {\n" +
+                            "            \"receiptNumber\": \"2\",\n" +
+                            "            \"controlNumber\": \"2\",\n" +
+                            "            \"receiptDate\": \"2019-04-19\",\n" +
+                            "            \"locationNumber\": 2,\n" +
+                            "            \"businessStatusCode\": \"A\",\n" +
+                            "            \"meta\": {\n" +
+                            "                \"unitofWorkid\": null,\n" +
+                            "                \"sorRoutingCtx\": {\n" +
+                            "                    \"replnTypCd\": null,\n" +
+                            "                    \"invProcAreaCode\": 36,\n" +
+                            "                    \"locationCountryCd\": \"US\"\n" +
+                            "                }\n" +
+                            "            }\n" +
+                            "        }\n" +
+                            "    ]\n" +
+                            "}"))
+                    .andReturn();
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void updateSummaryAndLineTest() {
-        ReceivingSummaryLineRequest receivingSummaryLineRequest = new ReceivingSummaryLineRequest("0", "0", LocalDate.now(), 0, "A", 1, "0", null);
+        ReceivingSummaryLineRequest receivingSummaryLineRequest = new ReceivingSummaryLineRequest("2", "2", LocalDate.now(), 2, "A", 1, "0", null);
         SuccessMessage successMessage = new SuccessMessage();
         List<ReceivingSummaryLineRequest> responseList = new ArrayList<>();
         successMessage.setMessage(true);
@@ -139,7 +209,63 @@ public class ReceivingSummaryControllerTest {
         successMessage.setData(responseList);
         successMessage.setTimestamp(LocalDateTime.now());
         Mockito.when(receiveSummaryServiceImpl.updateReceiveSummaryAndLine(receivingSummaryLineRequest, "US")).thenReturn(successMessage);
-        Assert.assertEquals(receiveSummaryServiceImpl.updateReceiveSummaryAndLine(receivingSummaryLineRequest,"US").getData(),successMessage.getData());
+        String body = new String("{\n" +
+                "            \"receiptNumber\": \"2\",\n" +
+                "            \"controlNumber\": \"2\",\n" +
+                "            \"receiptDate\": \"2019-04-19\",\n" +
+                "            \"locationNumber\": 2,\n" +
+                "            \"businessStatusCode\": \"A\",\n" +
+                "            \"inventoryMatchStatus\": \"9\",\n" +
+                "            \"meta\": {\n" +
+                "                \"unitofWorkid\": null,\n" +
+                "                \"sorRoutingCtx\": {\n" +
+                "                    \"replnTypCd\": null,\n" +
+                "                    \"invProcAreaCode\": 36,\n" +
+                "                    \"locationCountryCd\": \"US\"\n" +
+                "                }\n" +
+                "             }\n" +
+                "}");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/US/receiving/summary")
+                .header("X-FDS-FOUNDATION-API-KEY", "razorbacks")
+                .header("Content-Type", "application/json")
+                .header("X-IBM-Client-Secret", "rK7xP5iW4uV0gI4mA0dM8yQ3dV8tW2hG6nB8uA5mY1kA4sQ8oB")
+                .header("X-IBM-Client-Id", "e684e483-dbba-41d5-a040-6043b48798f5")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(body)
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try {
+            mockMvc.perform(requestBuilder)
+                    .andExpect(content().json(body))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json("{\n" +
+                            "    \"message\": true,\n" +
+                            "    \"timestamp\": \"2019-06-19T14:29:17.812\",\n" +
+                            "    \"data\": [\n" +
+                            "        {\n" +
+                            "            \"receiptNumber\": \"2\",\n" +
+                            "            \"controlNumber\": \"2\",\n" +
+                            "            \"receiptDate\": \"2019-04-19\",\n" +
+                            "            \"locationNumber\": 2,\n" +
+                            "            \"businessStatusCode\": \"A\",\n" +
+                            "            \"inventoryMatchStatus\": \"9\",\n" +
+                            "            \"sequenceNumber\": \"null\",\n" +
+                            "            \"meta\": {\n" +
+                            "                \"unitofWorkid\": null,\n" +
+                            "                \"sorRoutingCtx\": {\n" +
+                            "                    \"replnTypCd\": null,\n" +
+                            "                    \"invProcAreaCode\": 36,\n" +
+                            "                    \"locationCountryCd\": \"US\"\n" +
+                            "                }\n" +
+                            "            }\n" +
+                            "        }\n" +
+                            "    ]\n" +
+                            "}"))
+                    .andReturn();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
