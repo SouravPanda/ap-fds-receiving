dea23ecf0a4b9e5c49038353fe36a3298e648de4
andhuabh007 <anshuabhishekiitk007@gmail.com>
2019-07-19 11:23:30 +0530

    added vault capablity into projecte

--- src/main/java/com/walmart/finance/ap/fds/receiving/config/MongoConfiguration.java
+++ src/main/java/com/walmart/finance/ap/fds/receiving/config/MongoConfiguration.java
@@ -1,4 +1,39 @@
 package com.walmart.finance.ap.fds.receiving.config;
 
-public class MongoConfiguration {
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+@Configuration
+public class MongoConfiguration extends AbstractMongoConfiguration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MongoConfiguration.class);
+
+    @Value("${receiving-${spring.profiles.active}-${COUNTRY_NAME}}")
+    private String mongoURI;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+
+    @Override
+    public MongoClient mongoClient() {
+        LOG.debug("domain name is {} and country is {} ", System.getenv("SPRING_DOMAIN_NAME"),System.getenv("COUNTRY_NAME"));
+        MongoClientOptions.Builder optionsBuilder = new MongoClientOptions.Builder();
+        optionsBuilder.maxConnectionIdleTime(600000);
+        LOG.debug("mongo uri {}",mongoURI);
+        MongoClientURI uri = new MongoClientURI(mongoURI, optionsBuilder);
+        return  new MongoClient (uri);
+
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
 }
