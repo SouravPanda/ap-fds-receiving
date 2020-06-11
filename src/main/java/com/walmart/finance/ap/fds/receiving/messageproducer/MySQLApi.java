f1c6df0974f8b24fcc5891ab0823722d53ac71bc
j0b03kj <Joshna.Bantha@Walmartlabs.com>
2020-06-11 16:41:16 +0530

    fix codesmells

--- src/main/java/com/walmart/finance/ap/fds/receiving/messageproducer/MySQLApi.java
+++ src/main/java/com/walmart/finance/ap/fds/receiving/messageproducer/MySQLApi.java
@@ -43,11 +43,11 @@ public class MySQLApi {
         auditFailureRequest.setMeta(meta);
         auditFailureRequest.setOperation(message.getOperation());
         try {
-            ResponseEntity<AuditFailureResponse> AuditFailureResponse = auditAPIService.saveRecordsForAudit(auditFailureRequest);
-            log.info("Successfully updated the MySQL failure table with the failure record " + auditFailureRequest + AuditFailureResponse);
+            ResponseEntity<AuditFailureResponse> auditFailureResponse = auditAPIService.saveRecordsForAudit(auditFailureRequest);
+            log.info("Successfully updated the MySQL failure table with the failure record  %s and the response is %s",auditFailureRequest,auditFailureResponse);
 
         } catch (Exception exe) {
-            log.error("exception while calling Audit API to save the failure record to MySQL failure table  " + exe);
+            log.error("exception while calling Audit API to save the failure record to MySQL failure table %s ",exe );
         }
     }
 }
